43c6edeb5434f44b81cc74a90ec2915212f51f7d
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2019-06-17 07:50:50 +0530

    updated constants class and created deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,13 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class DealsPage extends BasePage{
+	
+	WebDriver driver;
+	public DealsPage(WebDriver driver){
+		this.driver = driver;
+	}
+
+	
+}
